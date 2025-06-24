515a1d84404daf0716d0c26150f6bd7de7e59db9
leonardodossantos2012 <leonardodossantos2012@hotmail.com>
2025-06-23 22:21:56 -0300

    update headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     
     /* Record video on failure */
     video: 'retain-on-failure',
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
